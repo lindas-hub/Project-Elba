4e87f47756f93ff5b0e4afcff188220c641a6a82
Mirlinda Zhushi <mirlindazh@gmail.com>
2020-07-08 13:05:49 +0000

    dlporid

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE>
+<html>
+    <head>
+	       <title> X Shoper</title>
+		   
+          
+		  
+		  <link rel="stylesheet" href="styles/style.css" media="all" />	
+	</head>
+	
+	<body>
+	
+	     <div class="main_weapper">
+		 
+		          <div class="header_wrapper"> 
+			 
+			     <img id="logo" src="images/logo.jpg" />
+				 <img id="banner" src="images/ad_banner.gif" />
+				 
+	
+	
+			 <div></div>
+			 </div>
+			 
+			 <div class="content_wrapper">
+			 
+			      <div id="sidebar">This is sidebar</div>
+			  
+			      <div id="content_area">This is content area</div>
+			 
+			 </div>
+			 
+			 
+			 
+			 <div>This is the foofedfeter</div>
+			 
+			 
+			 
+			 
+			 
+			 </div>
+		 
+		 
+			
+	
+	
+	
+	
+	</body>
+	</html>
