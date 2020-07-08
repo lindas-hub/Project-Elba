caebdc534a17bfaa002e52de40c345ef1ee963bc
Mirlinda Zhushi <mirlindazh@gmail.com>
2020-07-08 18:00:17 +0000

    kdmkdm

---
+++ functions/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","ecommerce");
+
+//getting the categories
+
+
+function getCats() {
+
+    global $con;
+
+    $get_cats = "select * from categories";
+
+    $run_cats = "mysqli_query"($con, $get_cats);
+
+    while ($row_cats=mysqli_fetch_array($run_cats) ) {
+
+        $cat_id = $row_cats['cat_id'];
+        $cat_title = $row_cats['cat_title'];
+    }
+
+
+    echo "<li><a href='#'>$cat_title</a></li>";
+}
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE>
+<?php
+
+include("functions/functions.php");
+
+?>
+
 <html>
     <head>
-	       <title> X Shoper</title>
+	       <title>Buy Online</title>
 		   
-          
-		  
-		  <link rel="stylesheet" href="styles/style.css" media="all" />	
+		   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+		   
+		   <link rel="stylesheet" href="styles/style.css" media="all" />	
 	</head>
 	
 	<body>
@@ -14,11 +20,14 @@
 	<div class="main_weapper">
 		 
 		         <!--Header starts here-->
-				  <div class="header_wrapper"> 
+				  <div class="row header_wrapper"> 
 			 
-			     <img id="logo" src="images/logo.jpg" />
-				 <img id="banner" src="images/ad_banner.jpg" />
+			     <img id="logo" class="col" style="height:150px" src="images/logo.jpg" />
+				 <img id="banner" class="col"  style="height:150px" src="images/ad_banner.jpg" />
+			     <img id="logo" class="col" style="height:150px" src="images/logo.jpg" />
+
 				 </div>
+				 <br/>
 				 <!--Header ends here--> 
 
 			     <!--Navigation Bar starts here -->
@@ -34,13 +43,41 @@
 						 <li><a href="#">Shopping Cart</a></li>
 						 <li><a href="#">Contact Us</a></li>
 						</ul>
+
+				<div id="form">
+					<form method="get" action="results.php" enctype="multipart/form-data">
+						<input type="text" name="user_query" placeholder="Search a Product" />
+						<input type="submit" name="search" value="Search" /> 
+					</form>
+				 </div>
 				 </div>
 				 <!--Navigation Ends here --> 
 
 				 <!--Content wrapper starts-->
 				 <div class="content_wrapper">
 			 
-			      <div id="sidebar">This is sidebar</div>
+				  <div id="sidebar">
+
+				      <div id="sidebar_title">Categories</div>
+					  
+					  <ul id="cats">
+
+					    <?php getCats(); ?>
+						 
+					     <ul>
+						
+						 <div id="sidebar_title">Brands</div>
+					  
+					  <ul id="cats">
+
+					     <li><a href="#">Pandora</a></li>
+						 <li><a href="#">Tiffany & Co</a></li>
+						 <li><a href="#">Cartier</a></li>
+						 <li><a href="#">Bvlgari</a></li>
+						 
+					     <ul>
+
+					  </div>
 			  
 			      <div id="content_area">This is content area</div>
 			 
